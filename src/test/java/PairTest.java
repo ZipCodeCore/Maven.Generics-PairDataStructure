90bb4b59bd7aa0a58bb420860f13e406d8e12884
Zach Marcin <marcinx27@gmail.com>
2017-05-15 23:15:11 -0400

    Rewrote the tests for the assignment, so that the students can extend the original Pair class

--- src/test/java/PairTest.java
+++ src/test/java/PairTest.java
@@ -2,48 +2,51 @@ import org.junit.Test;
 
 import org.junit.Assert;
 
+/**
+ * Merely getting these tests to pass is all you need to do, but that should be a quick assignment.
+ * Really mess around with the type parameters and see what happens.
+ * One example is in the `testGetters()` method, change the String type parameter to Integer and then run it.
+ *   What happens?
+ * Then uncomment the println and see what happens.
+ * Really try and digest what's happening behind the scenes.
+ */
 public class PairTest {
+
     @Test
-    public void testGetFirst() throws Exception {
-        Pair<String> p = new Pair("Foo", "Bar");
+    public void testGetters() throws Exception {
+        Pair<String, Integer> p = new Pair("Foo", 1);
+//        System.out.println(p.getFirst().getClass());
         Assert.assertEquals("Foo", p.getFirst());
+        Assert.assertEquals(new Integer(1),  p.getSecond());
     }
 
     @Test
-    public void testGetSecond() throws Exception {
-        Pair<String> p = new Pair("Foo", "Bar");
-        Assert.assertEquals("Bar", p.getSecond());
+    public void testSetters() throws Exception {
+        Pair<Integer, String> p = new Pair();
+        p.setFirst(new Integer(1));
+        p.setSecond("foobar");
+        Assert.assertEquals(new Integer(1), p.getFirst());
+        Assert.assertEquals("foobar", p.getSecond());
     }
 
     @Test
-    public void testSetFirst() throws Exception {
-        Pair<String> p = new Pair();
-        p.setFirst("Foo");
-        Assert.assertEquals("Foo", p.getFirst());
-        Assert.assertEquals(null, p.getSecond());
+    public void testNoTypeDeclaration() throws Exception {
+        Pair p = new Pair(1, "Foobar");
+        Assert.assertEquals(1, p.getFirst());
+        Assert.assertEquals("Foobar", p.getSecond());
+        p.setSecond(2.34);
+        Assert.assertEquals(2.34, p.getSecond());
     }
 
     @Test
-    public void testSetSecond() throws Exception {
-        Pair<String> p = new Pair();
-        p.setSecond("Bar");
-        Assert.assertEquals(null, p.getFirst());
-        Assert.assertEquals("Bar", p.getSecond());
-    }
-
-    @Test
-    public void testOverwriting() throws Exception {
-        Pair<String> p = new Pair();
-        Assert.assertEquals(null, p.getFirst());
-        Assert.assertEquals(null, p.getSecond());
-        p.setFirst("Foo");
-        p.setSecond("Bar");
-        Assert.assertEquals("Foo", p.getFirst());
-        Assert.assertEquals("Bar", p.getSecond());
-        p.setFirst("Zip");
-        p.setSecond("Code");
-        Assert.assertEquals("Zip", p.getFirst());
-        Assert.assertEquals("Code", p.getSecond());
+    public void testNoTypeDecAndSetters() throws Exception {
+        Pair p = new Pair();
+        p.setFirst("FooBar");
+        p.setSecond(2);
+        Assert.assertEquals("FooBar", p.getFirst());
+        Assert.assertEquals(2, p.getSecond());
+        p.setFirst(1);
+        Assert.assertEquals(1, p.getFirst());
     }
 
 }
